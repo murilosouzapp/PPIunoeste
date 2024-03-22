c00c8c122234680b483f0006f3bf71ea40ec01a3
murilosouzapp <164193095+murilosouzapp@users.noreply.github.com>
2024-03-22 12:21:45 -0300

    Atualizando meu projeto

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Ola mundo');
+import express, { application } from 'express';
+
+const host ='0.0.0.0';
+const porta = 3000;
+
+const app = express();
+
+app.get('/', (pega, devolve)=>{
+    devolve.write('<h1>Seja bem - vindo ao nosso site</h1>');
+    devolve.end();
+    
+
+});
+
+
+app.listen(porta, host, ()=>{
+    console.log(`Servido escutando em https://${host}:${porta}`);
+
+
+})
